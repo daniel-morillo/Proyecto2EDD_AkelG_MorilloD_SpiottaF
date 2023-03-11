0d80044a28c5f71b91d604be30f2fbc3f2ac85c7
faspiru <fabrizio.spiotta@correo.unimet.edu.ve>
2023-03-11 13:39:16 -0400

    txt listo

---
+++ MetroMendeley/src/Clases/Main.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+import Interfaces.InterfazInicial;
+
+/**
+ * Esta clase contiene el main que la da inicio a todo el proyecto
+ * @author Fabrizio Spiotta, Georgina Akel, Daniel Morillo
+ */
+public class Main {
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        InterfazInicial main = new InterfazInicial();
+        main.show();
+    }
+}
